f928848b004d29dbb97c2b96351ab67cb634714c
mssmac <mssmac@users.noreply.github.com>
2022-02-05 22:08:27 -0500

    new rmd file

--- teens-and-tech-cleaning.R
+++ teens-and-tech-cleaning.R
@@ -4,3 +4,12 @@ library(readr)
 df <- read_csv("March 7-April 10, 2018 - Teens and Tech Survey - CSV.csv")
 
 summary(df)
+
+#code rules: 
+# 77 = "don't know"
+# 98 = DK/Refusal
+# I need to look through the variables that are of interest to me and recreate a clean dataset with just those variables
+# review the survey topline/crosstabs documents for ideas
+
+
+#
